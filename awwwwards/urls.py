641bd44f525d1722f38049acb8341ea1f043d1e7
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-11-02 23:28:38 +0300

    cloudinary

--- awwwwards/urls.py
+++ awwwwards/urls.py
@@ -1,21 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+from .views import PostDetailView, PostCreateView
 from . import views
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('users', views.UserViewSet)
-router.register('posts', views.PostViewSet)
-router.register('profile', views.ProfileViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup/', views.signup, name='signup'),
-    path('account/', include('django.contrib.auth.urls')),
-    path('api/', include(router.urls)),
-    path('<username>/profile', views.user_profile, name='userprofile'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('profile/<username>/', views.profile, name='profile'),
-    path('profile/<username>/settings', views.edit_profile, name='edit'),
-    path('project/<post>', views.project, name='project'),
-    path('search/', views.search_project, name='search'),
+    path('', views.index, name='awward-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
